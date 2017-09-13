7b4c5f82f1744bb54e5a90b204722f350bac0758
riyajoseph4 <riyajoseph4@gmail.com>
2017-09-13 16:24:09 +0530

    validate phone number

--- num_check.rb
+++ num_check.rb
@@ -1,26 +1,29 @@
 class NumCheck
   
   def method_missing(method_name, *args, &block)
-     @numString = method_name[/\d+/].to_s 
-    
-     @lengthOfNum = @numString.length
-    
-     if @lengthOfNum == 11
+    @numString = method_name[/\d+/].to_s 
+    @string = method_name[5..method_name.length]
+    @lengthOfNum = @numString.length
+    if @numString !=@string
+      return "invalid number"
+    else
+      if @lengthOfNum == 11
         if @numString.start_with? '0484' 
            return "valid number"
         else
            return "invalid number"
         end
         
-     elsif @lengthOfNum == 10
+      elsif @lengthOfNum == 10
         if (@numString.start_with? '7')||(@numString.start_with? '8' )|| 
                                               (@numString.start_with? '9')
            return "valid number"
         else
            return  "invalid number"
         end
-    else
+      else
         return "invalid number"        
-    end            
-   end
+      end            
+    end
+  end
 end
