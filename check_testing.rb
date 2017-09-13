50e201afb4f5a7a2c8584a2348fae3f58c1113a2
riyajoseph4 <riyajoseph4@gmail.com>
2017-09-13 15:27:17 +0530

    validate a number

--- check_testing.rb
+++ check_testing.rb
@@ -1,35 +1,46 @@
 require "minitest/autorun"
 
 require "minitest/pride"
-require_relative "pgm"
+require_relative "num_check"
 
 class CallTest < Minitest::Test
   
   def setup 
-    @num1 = CallCheck.new("99887654356786")
-    @num2 = CallCheck.new("04844011845")
-    @num3 = CallCheck.new("8547810845")
-    @num4 = CallCheck.new("12321")
-    @num5 = CallCheck.new("9495969234")
+    @num1 = NumCheck.new.call_898989898989
+    @num2 = NumCheck.new.call_04844011845
+    @num3 = NumCheck.new.call_8547810845
+    @num4 = NumCheck.new.call_12321
+    @num5 = NumCheck.new.call_8547810845_call 
+    @num6 = NumCheck.new.call_call_8547810845
   end
 
-  def test_call_99887654356786
-    assert_equal "invalid number", @num1.call 
+  def test_call_898989898989
+    assert_equal "invalid number",@num1
   end
 
   def test_call_04844011845
-    assert_equal "valid number", @num2.call
+    assert_equal "valid number",@num2
   end
 
   def test_call_8547810845
-    assert_equal "valid number", @num3.call
+    assert_equal "valid number",@num3
   end
 
   def test_call_12321
-    assert_equal "invalid number", @num4.call
+    assert_equal "invalid number",@num4
   end
-  def test_call_9495969234
-    assert_equal "valid number", @num5.call
+
+  def test_call_8547810845_call  
+    assert_equal "valid number", @num5
+  end 
+
+  def test_call_call_8547810845  
+    assert_equal "valid number", @num6
   end
 
 end
+     
+
+
+
+
